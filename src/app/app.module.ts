92b0a33582f2998a2561b8bc263a2388895fb464
Your Name <you@example.com>
2020-05-04 00:11:52 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FooterComponent,
+    HomeComponent,
+    IntroComponent,
+    NavComponent,
+    ProductComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
